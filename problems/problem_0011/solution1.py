2b3b85d5551b735f2431a9340f2dde7691b14fb3
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2023-03-09 10:34:58 +0100

    feat: problem 0011

---
+++ problems/problem_0011/solution1.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        res = 0
+        while left < right:
+            res = max(res, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return res
+
+
+tests = [
+    (
+        ([1, 8, 6, 2, 5, 4, 8, 3, 7],),
+        49,
+    ),
+    (
+        ([1, 1],),
+        1,
+    ),
+    (
+        ([4, 3, 2, 1, 4],),
+        16,
+    ),
+]
