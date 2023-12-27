d430d7020ab276bf0d0c42574ecf0074cc419bf8
TurtleSmoke <46165291+TurtleSmoke@users.noreply.github.com>
2023-12-27 15:41:18 +0100

    feat: problem 0032

---
+++ problems/problem_0032/solution_1.py
@@ -0,0 +1,64 @@
+import pytest
+
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack_left = []
+        res = [0] * len(s)
+
+        for i, c in enumerate(s):
+            if c == "(":
+                stack_left.append(i)
+            else:
+                if stack_left:
+                    j = stack_left.pop()
+                    res[j] = i - j + 1
+
+        print(res)
+        max_len = 0
+        i = 0
+        while i < len(res):
+            j = i
+            while j < len(res) and res[j] != 0:
+                j += res[j]
+
+            if i != j:
+                max_len = max(max_len, j - i)
+
+            i = j + 1
+
+        return max_len
+
+
+tests = [
+    (
+        ("(()",),
+        2,
+    ),
+    (
+        (")()())",),
+        4,
+    ),
+    (
+        ("",),
+        0,
+    ),
+    (
+        ("(())()",),
+        6,
+    ),
+    (
+        ("(())()))(())()",),
+        6,
+    ),
+]
+
+
+@pytest.mark.timeout(2)
+@pytest.mark.parametrize(
+    "inputs, expected",
+    tests,
+)
+def test_validator(inputs, expected):
+    output = Solution().longestValidParentheses(*inputs)
+    assert output == expected
