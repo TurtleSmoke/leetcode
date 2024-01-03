2cb78900449c70b94f1ebd246cbbec483c42b860
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2024-01-03 19:45:38 +0100

    daily

---
+++ problems/problem_2125/solution_1.py
@@ -0,0 +1,32 @@
+import math
+
+import pytest
+from typing import List
+
+
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
+        bank = [v for v in (sum(c == "1" for c in line) for line in bank) if v > 0]
+        return sum(math.prod(devices) for devices in zip(bank, bank[1:]))
+
+
+tests = [
+    (
+        (["011001", "000000", "010100", "001000"],),
+        8,
+    ),
+    (
+        (["000", "111", "000"],),
+        0,
+    ),
+]
+
+
+@pytest.mark.timeout(2)
+@pytest.mark.parametrize(
+    "inputs, expected",
+    tests,
+)
+def test_validator(inputs, expected):
+    output = Solution().numberOfBeams(*inputs)
+    assert output == expected
