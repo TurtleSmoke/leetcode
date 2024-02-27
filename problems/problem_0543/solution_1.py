e35140b57b9c65a9d8db0fbcc49d589b8445e098
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2024-02-27 21:57:00 +0100

    daily

---
+++ problems/problem_0543/solution_1.py
@@ -0,0 +1,43 @@
+import pytest
+from typing import Optional
+
+from problems.utils import TreeNode
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+
+        def dfs(node):
+            nonlocal res
+            if not node:
+                return 0
+            left, right = dfs(node.left), dfs(node.right)
+            res = max(res, left + right)
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res
+
+
+tests = [
+    (
+        ([1, 2, 3, 4, 5],),
+        3,
+    ),
+    (
+        ([1, 2],),
+        1,
+    ),
+]
+
+
+@pytest.mark.timeout(2)
+@pytest.mark.parametrize(
+    "inputs, expected",
+    tests,
+)
+def test_validator(inputs, expected):
+    input = TreeNode.list_to_TreeNode(inputs[0])
+    output = Solution().diameterOfBinaryTree(input)
+    assert output == expected
