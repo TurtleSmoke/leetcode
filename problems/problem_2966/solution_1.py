0bf0381c1c0ad02cab54022230cf50710fe2b841
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2025-06-18 19:07:50 +0200

    daily

--- problems/problem_2966/solution_1.py
+++ problems/problem_2966/solution_1.py
@@ -5,9 +5,12 @@ from typing import List
 class Solution:
     def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
         nums = sorted(nums)
-        if any(abs(nums[i] - nums[i + 2]) > k for i in range(0, len(nums), 3)):
-            return []
-        return [nums[i : i + 3] for i in range(0, len(nums), 3)]
+        res = []
+        for i in range(0, len(nums), 3):
+            if nums[i + 2] - nums[i] > k:
+                return []
+            res.append(nums[i : i + 3])
+        return res
 
 
 tests = [
@@ -16,17 +19,27 @@ tests = [
         [[1, 1, 3], [3, 4, 5], [7, 8, 9]],
     ),
     (
-        ([1, 3, 3, 2, 7, 3], 3),
+        ([2, 4, 2, 2, 5, 2], 2),
         [],
+    ),
+    (
+        ([4, 2, 9, 8, 2, 12, 7, 12, 10, 5, 8, 5, 5, 7, 9, 2, 5, 11], 14),
+        [[2, 2, 2], [4, 5, 5], [5, 5, 7], [7, 8, 8], [9, 9, 10], [11, 12, 12]],
     ),
 ]
 
 
-@pytest.mark.timeout(2)
+# @pytest.mark.timeout(2)
 @pytest.mark.parametrize(
     "inputs, expected",
     tests,
 )
 def test_validator(inputs, expected):
+    _, k = inputs
     output = Solution().divideArray(*inputs)
-    assert output == expected
+    for triplet in output:
+        assert len(triplet) == 3, output
+        assert triplet[1] - triplet[0] <= k, output
+        assert triplet[2] - triplet[0] <= k, output
+        assert triplet[2] - triplet[1] <= k, output
+    assert len(output) == len(expected), output
